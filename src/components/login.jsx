e29de29aeb35165a5ed2184d7150a23189914353
Messudd <90100309+Messudd@users.noreply.github.com>
2024-02-12 02:15:17 +0300

    login_page_added

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/login.css";
 
 const Login = () => {
   return (
-    <div style={{color: '#fff'}}>Login yap !</div>
-  )
-}
+    <div className="login-page">
+      <div className="login-con">
+        <h1>Login</h1>
+        <form className="log-form">
+          <input type="email" placeholder="Email..." />
+          <input type="password" placeholder="Password..." />
+          <p>
+            <span>Don't you have an account ?</span>
+            <Link to="/sign_up">Sign Up</Link>
+          </p>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
